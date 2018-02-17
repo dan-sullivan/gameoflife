aba3cbbf44bba4c394b6e0dce0ffc2186b470127
Dan Sullivan <dan.sullivan.dev@gmail.com>
2018-02-17 06:52:54 +0000

    switch x and y in universe interactions. Was wrong

--- gol.py
+++ gol.py
@@ -1,7 +1,7 @@
 import random
 from os import system
 
-w, h = 50, 50
+w, h = 80, 25
 c_char = "o"
 
 universe = [[ bool(random.getrandbits(1)) for x in range(w)] for y in range(h)]
@@ -19,19 +19,19 @@ while True:
             for nx, ny in neighbour_coords:
                 # Are we overstepping the bounds of our universe?
                 if nx not in [-1,w] and ny not in [-1,h]:
-                    neighbour_count += int(universe[nx][ny])
+                    neighbour_count += int(universe[ny][nx])
             # Are we alive or dead?
-            if universe[x][y]:
+            if universe[y][x]:
             # Live cell
                 # Underpopulation and overpopulation = death :(
                 # 2 or 3 = lives on (we're already True)
                 if neighbour_count < 2 or neighbour_count > 3:
-                    universe[x][y] = False
+                    universe[y][x] = False
             else:
             # Dead Cell
                 # Reproduction
                 if neighbour_count == 3:
-                    universe[x][y] = True
+                    universe[y][x] = True
         # Add row to universe frame
         universe_frame += "".join([c_char if c else " " for c in universe[y]]) + "\n"
     # Print frame
